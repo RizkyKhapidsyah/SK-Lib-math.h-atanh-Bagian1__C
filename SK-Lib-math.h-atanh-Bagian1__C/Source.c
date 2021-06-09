856b98caee09e963b06bbb42f88d389bdbf74490
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-06-09 11:27:02 +0700

    Add project files.

---
+++ SK-Lib-math.h-atanh-Bagian1__C/Source.c
@@ -0,0 +1,17 @@
+/* atanh example */
+#include <stdio.h>      /* printf */
+#include <math.h>       /* tanh, atanh */
+#include <conio.h>
+
+/*	Source by CPlusPlus
+	Modified For Learn by Rizky Khapidsyah
+	I.D.E : VS2019 */
+
+int main() {
+	double param, result;
+	param = tanh(1);
+	result = atanh(param);
+	printf("The area hyperbolic tangent of %f is %f.\n", param, result);
+	_getch();
+	return 0;
+}
